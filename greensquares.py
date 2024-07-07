a0397ce1fa5f4fdba1bf6b7bdee5323cddf93262
Damien Boisvert <damien@alphagame.dev>
2024-07-06 17:23:44 -0700

    Comments and misc improvements

--- greensquares.py
+++ greensquares.py
@@ -14,10 +14,20 @@ from time import sleep
 # GIT_MODIFIED_FILE - The file to update (to make commits to) (default: file.txt)
 # GIT_COMMIT_INTERVAL - The amount of time between 'git commit'
 # GIT_COMMIT_PREFIX - The prefix to use with `git commit` messags. (default: "gsplusplus: ")
+# DISCORD_NOTIFICATIONS_WEBHOOK - discord webhook for notifications and info
 def mkCommitMsg(msg):
     """Make the commit message with the correct prefix"""
     return "%s %s" % (os.getenv("GIT_COMMIT_PREFIX", "gsplusplus:"), msg)
-    
+
+def webhook(message):
+    webhook = os.getenv("DISCORD_NOTIFICATIONS_WEBHOOK", None)
+    useWebhook = (webhook != None)
+
+    if useWebhook:
+        post(webhook, {
+            "content": "**[ :green_square:++ ]** %s" % message
+        })
+
 def main() -> int:
     """Program Entrypoint Function
     Returns: `int` (Exit Code)"""
@@ -30,21 +40,64 @@ def main() -> int:
     local_dir = os.getenv("GIT_LOCAL_DIR", "./git_repository")
     changed_file = os.getenv("GIT_MODIFIED_FILE", "file.txt")
     repository = os.getenv("GIT_REPO", None)
+    webhookUrl = os.getenv("DISCORD_NOTIFICATIONS_WEBHOOK", None)
+    useWebhook = (webhookUrl != None)
+    prefix = os.getenv("GIT_COMMIT_PREFIX", None)
+    commitInterval = os.getenv("GIT_COMMIT_INTERVAL", 86400)
+    
+    sys.stdout.write("Hold on, verifying your configuration...\n")
 
-    sys.stdout.write("Hold on, verifying your configuration... ")
+    # ----- NO REPOSITORY SET -----
     if repository == None:
         sys.stdout.write("Hey!  The Git repository environment variable ($GIT_REPO) isn't set!\n")
         sys.stdout.write("Let's fix that: Just set that to the SSH (or HTTPS if you like that) clone link.\n")
         return 1
+
+    # ----- WILL A WEBHOOK BE USED? -----
+    sys.stdout.write("Checking if a Discord webhook will be used... ")
+    sys.stdout.write("yes.\n" if useWebhook else "no.\n")
+
+    # ----- CUSTOM GIT PREFIX -----
+    sys.stdout.write("Checking if a custom Git commit prefix will be used... ")
+    sys.stdout.write(
+        "yes. (%s)\n" % prefix if prefix != None else "no.\n"
+    ) 
+
+
+    # ----- SHOW GIT REPO -----
+    sys.stdout.write("Checking Git Repository... ")
+    sys.stdout.write("%s\n" % repository)
+
+    # ----- CHECK IF COMMIT INTERVAL IS OK -----
+    sys.stdout.write("Checking if $GIT_COMMIT_INTERVAL is valid... ")
+    try:
+        commitInterval = int(commitInterval)
+    except ValueError:
+        sys.stdout.write("no. (Can't be converted to an integer)\n")
+        return 1
+
+    if commitInterval < 0:
+        sys.stdout.write("no. (Number is negative)\n")
+        return 1
+    sys.stdout.write("ok.\n")
+
+    # ----- CHECK IF GIT_LOCAL_DIR EXISTS -----
+    sys.stdout.write("Checking if $GIT_LOCAL_DIR already exists... ")
     if os.path.isdir(local_dir):
+        sys.stdout.write("yes.\n")
         sys.stdout.write("Folder '%s' exists, destroying it... " % local_dir)
         shutil.rmtree(local_dir)
         sys.stdout.write("done.\n")
+    else:
+        sys.stdout.write("no.\n")
 
-    sys.stdout.write("done.\n")
+    # ----- CONFIG OK -----
+    sys.stdout.write("Configuration OK.\n")
+    
     sys.stdout.write("Cloning local copy... ")
     repo = Repo.clone_from(os.getenv("GIT_REPO"), local_dir)
     sys.stdout.write("done.\n")
+
     os.chdir(local_dir)
     if not os.path.isfile("git_repository/file.txt"): # ensure the file does exist
         sys.stdout.write("The file to commit to (%s) doesn't exist. Making it... " % changed_file)
@@ -54,6 +107,8 @@ def main() -> int:
         repo.index.commit(mkCommitMsg("Add the file"))
         sys.stdout.write("done.\n")
 
+    webhook("GreenSquares++ has started!")
+    
     while True:
         d = datetime.now()
         dstr = f"[{d.month}/{d.day}/{d.year} - {d.hour}:{d.minute}:{d.second}]"
@@ -63,7 +118,9 @@ def main() -> int:
         repo.index.add(changed_file)
         repo.index.commit(mkCommitMsg(f"Here be green squares. {d.month}/{d.day}/{d.year}"))
         sys.stdout.write("done.\n")
-        sleep(5)
+        webhook("Commit OK")
+        sleep(commitInterval)
+
     return 0
 
 # import run prevention
